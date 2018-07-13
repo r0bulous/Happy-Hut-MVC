5231f4c2fdf9e243a76aacdbc09f13a3b1880a89
r0bulous <rrwinn@gmail.com>
2018-07-12 22:33:02 -0400

    began styling, converted customer index page to bootstap

--- Happy Hut MVC/Models/Customer.cs
+++ Happy Hut MVC/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -11,12 +12,19 @@ namespace Happy_Hut_MVC.Models
     {
         [Key]
         public int CustomerID { get; set; }
+        [DisplayName("Customer Name")]
         public string CustomerName { get; set; }
+        [DisplayName("Email Address")]
         public string CustomerEmail { get; set; }
+        [DisplayName("Phone Number")]
         public string CustommerPhone { get; set; }
+        [DisplayName("Preferred Beverage")]
         public string FavDrink { get; set; }
+        [DisplayName("Preferred Meal")]
         public string FavMeal { get; set; }
+        [DisplayName("Max. Check Amount")]
         public decimal LargestBill { get; set; }
+        [DisplayName("Address")]
         public virtual ICollection<Address> Addresses { get; set; }
 
     }
